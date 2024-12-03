5cba3972def4b1f8de1ec388133ff90dd497fbe4
MarkAlfieM <mark@xtendly.com>
2024-12-03 08:07:30 +0800

    added widgets
    -Pagetitle
    -ImageWidget
    -DropdownWidget

--- go_moon/lib/pages/home_page.dart
+++ go_moon/lib/pages/home_page.dart
@@ -1,10 +1,73 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  late double _deviceHeight, _deviceWidgth;
   HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidgth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+            padding: EdgeInsets.symmetric(horizontal: _deviceHeight * 0.05),
+            height: _deviceHeight,
+            width: _deviceWidgth,
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // mainAxisSize: MainAxisSize.max,
+              // crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                _pageTitle(),
+                _destinationDropDownWidget(),
+              ],
+            )),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return Container(
+      color: Colors.green,
+      child: const Text(
+        "#GoMoon",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 70,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage("assets/images/astro_moon.png"))),
+    );
+  }
+
+  Widget _destinationDropDownWidget() {
+    List<String> _items = [
+      'James Webb Station',
+      'Preneure Station',
+    ];
+
+    return DropdownMenuItem(
+        child: DropdownButton(
+      underline: Container(),
+      onChanged: (_) {},
+      items: _items.map((e) {
+        return DropdownMenuItem(
+          child: Text(e),
+          value: e,
+        );
+      }).toList(),
+    ));
   }
 }
